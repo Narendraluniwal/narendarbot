b7077ac5ee8cb5b5469336c531c89e1426da1e15
narendraluniwal <nluniwal2@gmail.com>
2023-06-21 12:55:56 +0530

    Innitial Source

---
+++ narendar/__init__.py
@@ -0,0 +1,11 @@
+from telethon import TelegramClient
+import logging
+import time
+
+
+
+api_id="1125689"
+api_hash="4772d1792ed194020a8fb06a91ffb8fa"
+bot_token="6192130255:AAGsCcD7ZKYqsFOkqXZd9eyKHnVbclutM3g"
+
+bot = TelegramClient("narendar", api_id, api_hash).start(bot_token = bot_token)
